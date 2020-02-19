7e1735a656413e0f6fac025be2763a3329af03e6
yikuyirong <yikuyirong@126.com>
2020-02-19 15:29:03 +0800

    aaa

--- l20.py
+++ l20.py
@@ -5,14 +5,6 @@ def main():
 
      image = Image.new("RGB",size=(2970,2100),color=(255,255,255))
 
-     imagedraw = ImageDraw.Draw(image)
-
-     imagefont = ImageFont. . .load_default()
-     imagefont.
-
-     imagedraw.text((10,10),text="Hello world",fill="black",)
-
-
      image.show()
 
 
